5e98e0ca29d32d0107ee6ecd8e895986e8813495
Rudraksh Rathore <rudraksh1262@gmail.com>
2026-09-07 22:27:29 +0530

    Add files via upload

---
+++ swap_2_no.c
@@ -0,0 +1,11 @@
+//4. Program to Swap Two Numbers Using a Temporary Variable 
+#include <stdio.h> 
+int main() { 
+    int a, b, temp; 
+    printf("Enter two numbers: "); 
+    scanf("%d %d", &a, &b); 
+    printf("Before swapping: a = %d, b = %d\n", a, b); 
+    temp = a; a = b; b = temp; 
+    printf("After swapping: a = %d, b = %d\n", a, b); 
+    return 0; 
+} 
